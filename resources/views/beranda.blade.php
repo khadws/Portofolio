3f1d0e82a15d3a2046cc60a34e490d781ed419d4
khadws <dawwas.khaidirud@gmail.com>
2026-07-29 18:34:23 +0700

    update tittle

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Khaidirud Dawwas | Portofolio Front-End Developer</title>
+    <title>Khaidirud Dawwas | Portofolio</title>
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
